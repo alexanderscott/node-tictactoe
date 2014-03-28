633539be75f7bf2c0bc0331977ffda49db5f70c3
Alex Ehrnschwender <ehrndog@gmail.com>
2014-03-27 19:58:36 -0700

    tictactoe three in a row

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = require( path.resolve(__dirname, './lib/tictactoe' ));
---
+++ test/tictactoe-tests.js
@@ -0,0 +1,27 @@
+"use strict";
+
+exports.name = 'tictactoe-tests';
+
+var assert = require('assert'),
+    TicTacToe = require('../lib/tictactoe');
+
+
+describe(exports.name, function(){
+    before(function(cb){
+        cb();
+    });
+
+    beforeEach(function(cb){
+        cb();
+    });
+
+    afterEach(function(cb){
+        cb();
+    });
+
+    after(function(cb){
+        cb();
+    });
+
+});
+
